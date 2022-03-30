a1a2829e6f14cbf62c639f820646cedd19ed83cc
Alex <88835216+a4004@users.noreply.github.com>
2022-03-30 20:35:21 +0100

    Update libtime.js

--- src/lib/libtime.js
+++ src/lib/libtime.js
@@ -1,16 +1,11 @@
 const libowl = require('../lib/libowl');
-const moment = require('moment-timezone');
 
+// REMOVED TIMEZONE CONVERSIONS AS THEY'RE REDUNDANT, KEEPING USE OF FUNCTIONS FOR COMPATIBILITY WITH SOURCE CODE.
 module.exports = {
-    getTimeZone: function(time, zone) {
-        let z_time = new Date(moment(time.toISOString()).tz(zone).toISOString());
-        libowl.condlog('libtime'.cyan, 'Converting Time ' + time.toISOString().bgBlack + ' to ' + `${zone}`.bgBlack.cyan + ' ' + z_time.toISOString().bgBlack.green);
-        return z_time;
-    },
     getLondonTimeNow: function() {
         return this.getLondonTime(new Date(Date.now()));
     },
     getLondonTime: function(time) {
-        return this.getTimeZone(time, 'Europe/London');
+        return time;
     }
 }
